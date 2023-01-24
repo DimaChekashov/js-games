cc494dedf474a8a5c0d61022472aeaf0900a0d7a
DimaChekashov <foxdima99@gmail.com>
2023-01-24 01:59:55 +0100

    improve rps game

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -2,51 +2,85 @@ const computerChoiceDisplay = document.querySelector("#computer-choice");
 const userChoiceDisplay = document.querySelector("#user-choice");
 const resultDisplay = document.querySelector("#result");
 const possibleChoices = document.querySelectorAll("button");
+const audio = document.querySelector("#audio");
 
 let userChoice;
 let computerChoice;
 
 possibleChoices.forEach((btn) =>
     btn.addEventListener("click", (e) => {
-        userChoice = e.target.id;
-        userChoiceDisplay.innerHTML = userChoice;
+        userChoice = btn.getAttribute("id");
+        showImg(userChoiceDisplay, userChoice);
         getComputerChoice();
         getResult();
     })
 );
 
+function showImg(box, type) {
+    box.innerHTML = `<img src="./assets/${type}.png" alt="${type}">`;
+}
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
 
-    if (randomNumber === 1) computerChoice = "rock";
-    if (randomNumber === 2) computerChoice = "paper";
-    if (randomNumber === 3) computerChoice = "scissors";
+    computerChoice =
+        randomNumber === 1 ? "rock" : randomNumber === 2 ? "paper" : "scissors";
 
-    computerChoiceDisplay.innerHTML = computerChoice;
+    showImg(computerChoiceDisplay, computerChoice);
 }
 
 function getResult() {
-    if (computerChoice === userChoice) {
+    if (
+        (computerChoice === "rock" && userChoice === "paper") ||
+        (computerChoice === "paper" && userChoice === "scissors") ||
+        (computerChoice === "scissors" && userChoice === "rock")
+    ) {
+        result = "You win!";
+    } else if (computerChoice === userChoice) {
         result = "it's a draw!";
-    }
-    if (computerChoice === "rock" && userChoice === "paper") {
-        result = "you win!";
-    }
-    if (computerChoice === "rock" && userChoice === "scissors") {
-        result = "you lose!";
-    }
-    if (computerChoice === "paper" && userChoice === "scissors") {
-        result = "you win!";
-    }
-    if (computerChoice === "paper" && userChoice === "rock") {
-        result = "you lose!";
-    }
-    if (computerChoice === "scissors" && userChoice === "rock") {
-        result = "you win!";
-    }
-    if (computerChoice === "scissors" && userChoice === "paper") {
-        result = "you lose!";
+    } else {
+        result = "You lose!";
     }
 
     resultDisplay.innerHTML = result;
 }
+
+// audio.muted = true;
+// audio.play();
+// audio.addEventListener("canplaythrough", function() {
+//     if(confirm("Turn on music?")) {
+//         audio.play();
+//     }
+//     document.body.addEventListener("mousemove", function () {
+//         audio.play();
+//     });
+// });
+
+let modal = document.createElement("div");
+modal.classList.add("modal-overlay");
+
+function showModal() {
+    modal.innerHTML = `
+        <div class="modal">
+            <div class="modal-title">Turn on music?</div>
+            <div>
+                <button type="button" onclick="onMusic(true)">Yes</button>
+                <button type="button" onclick="onMusic(false)">No</button>
+            </div>
+        </div>
+    `;
+    document.querySelector("body").appendChild(modal);
+}
+
+function hideModal() {
+    modal.remove();
+}
+
+function onMusic(status) {
+    if(status) {
+        audio.play();
+    }
+    hideModal();
+}
+
+showModal();
