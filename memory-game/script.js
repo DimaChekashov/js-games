f8879f155f04e5a5da3d1cd8de9f0f9a184dee24
DimaChekashov <foxdima99@gmail.com>
2023-02-01 17:29:53 +0100

    fix game

--- memory-game/script.js
+++ memory-game/script.js
@@ -8,28 +8,20 @@ const cardArray = [
         img: "images/cheeseburger.png",
     },
     {
-        name: "hotdog",
-        img: "images/hotdog.png",
-    },
-    {
         name: "ice-cream",
         img: "images/ice-cream.png",
-    },
-    {
-        name: "milkshake",
-        img: "images/milkshake.png",
     },
     {
         name: "pizza",
         img: "images/pizza.png",
     },
     {
-        name: "hotdog",
-        img: "images/hotdog.png",
+        name: "milkshake",
+        img: "images/milkshake.png",
     },
     {
-        name: "ice-cream",
-        img: "images/ice-cream.png",
+        name: "hotdog",
+        img: "images/hotdog.png",
     },
     {
         name: "fries",
@@ -40,8 +32,16 @@ const cardArray = [
         img: "images/cheeseburger.png",
     },
     {
+        name: "ice-cream",
+        img: "images/ice-cream.png",
+    },
+    {
         name: "pizza",
         img: "images/pizza.png",
+    },
+    {
+        name: "milkshake",
+        img: "images/milkshake.png",
     },
     {
         name: "hotdog",
